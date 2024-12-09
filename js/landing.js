3e7c5c532ae7656431fca269861e39efd5ea8044
Saif0990 <Saifamjadabusaad@gmail.com>
2024-12-08 23:15:20 -0500

    update
    add new JS ,CSS and html

--- js/landing.js
+++ js/landing.js
@@ -2,21 +2,12 @@ import Spheres2Background from 'https://cdn.jsdelivr.net/npm/threejs-components@
 
 const bg = Spheres2Background(document.getElementById('webgl-canvas'), {
   count: 200,
-  colors: [0xff0000, 0x0, 0xffffff],
+  colors: [0x00FF7B, 0x0, 0xffffff],
   minSize: 0.5,
   maxSize: 1
 })
 
-const button1 = document.getElementById('colors-btn')
 
-document.body.addEventListener('click', (ev) => {
-  if (ev.target !== button1) bg.togglePause()
-})
-
-button1.addEventListener('click', () => {
-  bg.spheres.setColors([0xffffff * Math.random(), 0xffffff * Math.random(), 0xffffff * Math.random()])
-  bg.spheres.light1.color.set(0xffffff * Math.random())
-})
 
 
 
